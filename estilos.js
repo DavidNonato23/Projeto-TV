72b9ba0aeedfc3747f0af64b2130f0c380a1fab0
DavidNonato23 <146906330+DavidNonato23@users.noreply.github.com>
2024-11-06 21:36:15 -0300

    Add files via upload

---
+++ estilos.js
@@ -0,0 +1,21 @@
+alert('Seja bem-vindo!');
+let usuario = 'admin';
+let chute
+
+alert('Sua senha ?')
+let senha = 'admin';
+let ida
+
+while (chute != usuario) {
+    chute = prompt('Qual Usuario ?');
+}
+
+while (ida != senha) {
+    ida = prompt('Qual sua senha ?');
+}  
+
+    if (saldo == chute ) {
+        alert('Cadrasto encontrado');
+    } else {
+        alert('Saldo insuficiente');
+    }
